a1317238177140fb9b320b97894700bf71fdf8dc
Antonio <altminati@gmail.com>
2016-03-25 20:55:03 -0600

    Adding project baseline.

---
+++ CompilerCSharp/Source/Compiler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompilerCSharp
+{
+    class Compiler
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# Pascal Compiler");
+            Console.Read();
+        }
+    }
+}
